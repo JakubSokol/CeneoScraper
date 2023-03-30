946fa991badd48307149509226de5e03779db6c8
jakubsokol <kubas.sokol@gmail.com>
2023-03-30 18:15:39 +0200

    2023-03-30 JS

---
+++ scraper.py
@@ -0,0 +1,9 @@
+from requests import get
+
+#product_code = input("Please enter product code: ")
+product_code="36991221"
+#print(product_code)
+
+url=f"https://www.ceneo.pl/{product_code}#tab=reviews"
+response = get(url)
+print(response.status_code)
